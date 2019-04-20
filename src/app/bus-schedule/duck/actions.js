f6ed13f54a0adde4738cfb73af99fd2b84efa659
gabriel leoni <gabeamaleoni@gmail.com>
2019-04-19 22:18:11 -0700

    assign each trip to its own bus

--- src/app/bus-schedule/duck/actions.js
+++ src/app/bus-schedule/duck/actions.js
@@ -1,7 +1,17 @@
 // bus-schedule/duck/actions.js
 import * as types from './types'
 
-export const onBusScheduleLoad = json => ({
+const acceptBusScheduleLoad = json => ({
 	type: types.ON_BUS_SCHEDULE_LOAD,
 	payload: json
 })
+export const onBusScheduleLoad = trips => dispatch => {
+	const updatedTrips = []
+	if (trips && trips.length) {
+		trips.forEach((trip, idx) => {
+			trip.busId = idx + 1
+			updatedTrips.push(trip)
+		})
+	}
+	dispatch(acceptBusScheduleLoad(updatedTrips))
+}
