167f13a770c423eb9b9c9fb6e44e9e38863fcf3d
gabriel leoni <gabeamaleoni@gmail.com>
2019-04-23 21:01:59 -0700

    Updates

---
+++ precache-manifest.66d854287582aceed7f06e563687f783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e406958c3d2da117f615",
+    "url": "/bus-scheduling/static/js/runtime~main.8619965a.js"
+  },
+  {
+    "revision": "348909896cab821223fb",
+    "url": "/bus-scheduling/static/js/main.ef5f4280.chunk.js"
+  },
+  {
+    "revision": "cc071ee4229225df7dba",
+    "url": "/bus-scheduling/static/js/2.703d1d12.chunk.js"
+  },
+  {
+    "revision": "348909896cab821223fb",
+    "url": "/bus-scheduling/static/css/main.518522bf.chunk.css"
+  },
+  {
+    "revision": "d61799d813a3abf8112c27fc9ae58249",
+    "url": "/bus-scheduling/index.html"
+  }
+];
